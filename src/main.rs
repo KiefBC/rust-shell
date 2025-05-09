5d30d516c9a131d774fd18b86496e70b40def557
KiefBC <kiefhay642@outlook.com>
2025-05-08 22:58:11 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     let stdin = io::stdin();
@@ -8,6 +9,9 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         stdin.read_line(&mut input).unwrap();
+        if input.trim() == "exit 0" {
+            process::exit(0x0100);
+        }
         println!("{}: command not found", input.trim());
     }
 }
